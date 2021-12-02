57c81a8a0d5dc8018aa9d3aaf9376cd78549b59f
Tobiasz Ciesielski <t.ciesielski1177@gmail.com>
2021-12-02 21:33:26 +0100

    day 1 part 2

--- day1.ts
+++ day1.ts
@@ -10,3 +10,23 @@ numbers.reduce((currentValue, nextValue) => {
 });
 
 console.log(increasedCount);
+
+// part 2
+
+let increasedWindowSumCount = 0;
+
+let index = 3;
+numbers.reduce((_, __) => {
+  const currentWindowSum = numbers
+    .slice(index - 3, index)
+    .reduce((number, nextNumber) => number + nextNumber);
+
+  const nextWindowSum = numbers
+    .slice(index - 2, index + 1)
+    .reduce((number, nextNumber) => number + nextNumber);
+
+  currentWindowSum < nextWindowSum && increasedWindowSumCount++;
+  return index++;
+});
+
+console.log(increasedWindowSumCount);
